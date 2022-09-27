de88d218f3ed4c7dc6f759dc4857741fd2187a93
Lauri <lauri.vastela@gmail.com>
2022-09-27 18:04:19 +0300

    UPDATES

--- KauppaEntity.py
+++ KauppaEntity.py
@@ -16,7 +16,8 @@ class Tables:
 
                     CREATE TABLE Asiakas (
                         As_ID  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-                        As_Nimi    TEXT
+                        As_Nimi    TEXT,
+                        As_numero TEXT
                     );
                     CREATE TABLE Tuote (
                         Tuote_ID  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -35,32 +36,42 @@ class Tables:
                     );
                     ''')
     
-
             #CREATE TABLES
             conn.executescript(queries)
 
             try:
                 conn
-                print('Uudelleenluonti onnistui')
-            
+                print('Uudelleenluonti onnistui')        
             except Error as e:
                 print(e)
-                
-                    
+                               
             #DATA TO DATABASE
             d = conn.cursor()
             d.executescript('''INSERT INTO Tuote (Tuote_nimi, Tuote_hinta)
-                VALUES ('Juusto', '2e')''') 
-            d.executescript('''INSERT INTO Asiakas (As_nimi)
-                VALUES ('John')''') 
+                            VALUES  ('Juusto', '2e'),
+                                    ('Maito', '2e'),
+                                    ('Leipä', '2e'),
+                                    ('Megaforce', '1e'),
+                                    ('Euroshopper energy drink', '5e'),
+                                    ('Mutakakku', '7e'),
+                                    ('Kakku', '12e')
+                                    ''') 
+
+            d.executescript('''INSERT INTO Asiakas (As_nimi, As_numero)
+                            VALUES  ('John', 040392123), 
+                                    ('Pekka', 023232323),
+                                    ('Jussi', 0231233332),
+                                    ('Pirkko', 0231441132),
+                                    ('Jerome', 0214412132),
+                                    ('Otto', 0232323425),
+                                    ('Pavel', 0231551342)
+                                    ''') 
 
             try:
                 d
-                print('Datan lisäys onnistui')
-            
+                print('Datan lisäys onnistui')      
             except Error as e:
-                print(e)
-            
+                print(e)    
         except Error as e:
                 print(e)
 
